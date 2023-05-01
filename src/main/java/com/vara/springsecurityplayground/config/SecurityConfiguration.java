d74866eaba2034a2c641f7723676691b9cbbd969
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-05-02 00:21:56 +0530

    add event listener on success auth

--- src/main/java/com/vara/springsecurityplayground/config/SecurityConfiguration.java
+++ src/main/java/com/vara/springsecurityplayground/config/SecurityConfiguration.java
@@ -3,9 +3,14 @@ package com.vara.springsecurityplayground.config;
 
 import com.vara.springsecurityplayground.filters.RobotFilter;
 import com.vara.springsecurityplayground.filters.VaraAuthenticationProvider;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -19,7 +24,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationEventPublisher authenticationEventPublisher) throws Exception {
+        {
+            httpSecurity.getSharedObject(AuthenticationManagerBuilder.class).authenticationEventPublisher(authenticationEventPublisher);
+        }
+
         return httpSecurity
                 .authorizeRequests()
                 .antMatchers("/marketing").permitAll()
@@ -38,6 +47,15 @@ public class SecurityConfiguration {
                 User.builder().username("user").password("{noop}password").authorities("ROLE_USER").build()
         );
 
+    }
+
+    @Bean
+    public ApplicationListener<AuthenticationSuccessEvent> successEventApplicationListener() {
+        return event -> {
+            System.out.println(
+                    String.format("success [%s] %s", event.getAuthentication().getClass().getName(), event.getAuthentication().getName())
+            );
+        };
     }
 }
 
